2f7f946a0536b55e79ae713d9f6afef0233e274d
Dominic Moylett <dm1905@bristol.ac.uk>
2014-09-02 14:43:33 +0100

    Added new test.
    This test sees the pattern period end.

--- parameterised_matching.c
+++ parameterised_matching.c
@@ -15,5 +15,10 @@ int main(void) {
     if (matches) printf("%d\n", results[matches - 1]);
     else printf("No matches\n");
 
+    matches = parameterised_match("aaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaabbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 200, "aaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaaaabbbbbaaaaacccccaaabb", 65, "abc", 3, 0, results);
+    for (i = 0; i < matches - 1; i++) printf("%d, ", results[i]);
+    if (matches) printf("%d\n", results[matches - 1]);
+    else printf("No matches\n");
+
     return 0;
 }
