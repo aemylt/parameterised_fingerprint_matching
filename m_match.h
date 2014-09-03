95f427181c077b70b3b2791af0d079bcc0c7e90f
Dominic Moylett <dm1905@bristol.ac.uk>
2014-09-03 16:41:18 +0100

    Failure table is now compressed.
    Still need to improve run time to O(\Sigma_P + \rho).

--- m_match.h
+++ m_match.h
@@ -44,20 +44,38 @@ int compare_pi_pj(int i, int j, int i_pred, int j_pred) {
 }
 
 /*
+    typedef struct failure_list_t failure_list
+    Double-Linked List of failure items.
+    Components:
+        failure_list_t *pred   - Predecessor item in the list
+        failure_list_t *succ   - Successor item in the list
+        int            start   - Start of range of indices with this failure value
+        int            failure - Failure value
+*/
+typedef struct failure_list_t {
+    struct failure_list_t *pred, *succ;
+    int start, failure;
+} failure_list;
+
+/*
     typedef struct mmatch_state
     Structure to hold current state of algorithm.
     Components:
-        int *k         - Length before predecessor changes from zero
-        int *c         - Value after predecessor changes
-        int m          - Length of pattern
-        int i          - Current index of pattern
-        int *failure   - Failure table for pattern
-        int period     - Length of period
-        int has_break  - Does the period break?
-        int pred_break - The predecessor of the character that breaks the period
+        int          *k             - Length before predecessor changes from zero
+        int          *c             - Value after predecessor changes
+        int          m              - Length of pattern
+        int          i              - Current index of pattern
+        int          *failure_table - Failure_table table for pattern
+        int          period         - Length of period
+        int          has_break      - Does the period break?
+        int          pred_break     - The predecessor of the character that breaks the period
+        int          failure_break  - The failure value of the character that breaks the period
+        failure_list *failure       - The current index in the failure list
+        failure_list *failure_reset - Pointer to the index in the failure list to reset to if the pattern is not periodic
 */
 typedef struct {
-    int *k, *c, m, i, *failure, period, has_break, pred_break;
+    int *k, *c, m, i, *failure_table, period, has_break, pred_break, failure_break;
+    failure_list *failure, *failure_reset;
 } mmatch_state;
 
 /*
@@ -73,10 +91,42 @@ typedef struct {
 int get_pred(mmatch_state state, int i) {
     if (!state.period) return state.k[i];
     if ((state.has_break) && (i == state.m - 1)) return state.pred_break;
-    else {
-        int index = i % state.period;
-        return (i < state.k[index]) ? 0 : state.c[index];
+    int index = i % state.period;
+    return (i < state.k[index]) ? 0 : state.c[index];
+}
+
+/*
+    get_failure
+    Gets the failure value for integer i.
+    Parameters:
+        mmatch_state *state - The state of the algorithm
+        int          i      - The index of the pattern
+    Returns int:
+        Length of the longest prefix that also p-matches a suffix.
+        Failure list in parameter state also updated.
+*/
+int get_failure(mmatch_state *state, int i) {
+    if (!state->period) return i - state->failure_table[i];
+    if ((state->has_break) && (i == state->m - 1)) {
+        state->failure = state->failure_reset;
+        return state->failure_break;
     }
+    int index = i - state->failure->failure;
+    if ((state->failure->pred != NULL) && (index < state->failure->start)) state->failure = state->failure->pred;
+    return index;
+}
+
+/*
+    update_failure
+    Updates the failure list.
+    Parameters:
+        mmatch_state *state - The state of the algorithm
+        int          i      - The index of the pattern
+    Returns void:
+        Failure list in parameter state updated.
+*/
+void update_failure(mmatch_state *state, int i) {
+    if ((state->period) && (state->failure->succ != NULL) && (i >= state->failure->succ->start)) state->failure = state->failure->succ;
 }
 
 /*
@@ -90,23 +140,24 @@ int get_pred(mmatch_state state, int i) {
         Initial state for algorithm
 */
 mmatch_state mmatch_build(int *p_pred, int m, int p_len) {
-    int i, j, k;
+    int i, j;
     mmatch_state state;
-    state.failure = malloc(m * sizeof(int));
-    i = -1;
+    state.failure_table = malloc(m * sizeof(int));
+    state.failure_table[0] = 1;
     state.k = malloc(m * sizeof(int));
-    state.failure[0] = 1;
     state.k[0] = 0;
+    i = -1;
     for (j = 1; j < m; j++) {
         state.k[j] = p_pred[j];
-        while (i > -1 && !compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i -= state.failure[i];
+        while (i > -1 && !compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i -= state.failure_table[i];
         if (compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i++;
-        state.failure[j] = j - i;
+        state.failure_table[j] = j - i;
     }
 
     state.has_break = 0;
-    if ((state.failure[m - 1] << 1) < m) {
-        state.period = state.failure[m - 1];
+    if ((state.failure_table[m - 1] << 1) <= m) {
+        int k, failure_j;
+        state.period = state.failure_table[m - 1];
         state.k = realloc(state.k, state.period * sizeof(int));
         state.c = malloc(state.period * sizeof(int));
 
@@ -123,25 +174,56 @@ mmatch_state mmatch_build(int *p_pred, int m, int p_len) {
             }
         }
 
+        state.failure = malloc(sizeof(failure_list));
+        state.failure->failure = 1;
+        state.failure->start = 0;
+        state.failure->pred = NULL;
+        state.failure->succ = NULL;
+        j = 0;
+        while ((j < m) && (state.failure->failure < state.period)) {
+            if (state.failure_table[j] != state.failure->failure) {
+                state.failure->succ = malloc(sizeof(failure_list));
+                state.failure->succ->pred = state.failure;
+                state.failure = state.failure->succ;
+                state.failure->failure = state.failure_table[j];
+                state.failure->start = j;
+                state.failure->succ = NULL;
+            }
+            j++;
+        }
+
+        while ((state.failure->pred != NULL) && (state.failure->pred->start >= i)) state.failure = state.failure->pred;
+
         j = m;
-        while ((j < p_len) && ((state.failure[j - 1] << 1) < m)) {
-            while (i > -1 && !compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i -= state.failure[i];
-            if (compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i++;
-            state.failure[j] = j - i;
-            if ((state.failure[j] << 1) >= m) {
+        failure_j = state.failure_table[j - 1];
+        free(state.failure_table);
+        while ((j < p_len) && ((failure_j << 1) < m)) {
+            while (i > -1 && !compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) i = get_failure(&state, i);
+            if (compare_pi_pj(i + 1, j, p_pred[i + 1], p_pred[j])) {
+                i++;
+                update_failure(&state, i);
+            }
+            failure_j = j - i;
+            if ((failure_j << 1) >= m) {
                 state.has_break = 1;
                 state.pred_break = p_pred[j];
+                state.failure_break = i;
+                state.failure_reset = state.failure;
             } else if ((state.c[j % state.period] == 0) && (p_pred[j] != 0)) {
                 state.c[j % state.period] = p_pred[j];
                 state.k[j % state.period] = j;
             }
             j++;
         }
+
+        while (state.failure->pred != NULL) state.failure = state.failure->pred;
+
     } else {
         state.period = 0;
     }
     state.m = j;
     state.i = -1;
+
     return state;
 }
 
@@ -157,13 +239,15 @@ mmatch_state mmatch_build(int *p_pred, int m, int p_len) {
         -1 otherwise
 */
 int mmatch_stream(mmatch_state *state, int t_pred, int j) {
-    int* failure = state->failure;
     int i = state->i, result = -1;
-    while (i > -1 && !compare_pi_tj(i + 1, t_pred, get_pred(*state, i + 1))) i -= failure[i];
-    if (compare_pi_tj(i + 1, t_pred, get_pred(*state, i + 1))) i++;
+    while (i > -1 && !compare_pi_tj(i + 1, t_pred, get_pred(*state, i + 1))) i = get_failure(state, i);
+    if (compare_pi_tj(i + 1, t_pred, get_pred(*state, i + 1))) {
+        i++;
+        update_failure(state, i);
+    }
     if (i == state->m - 1) {
         result = j;
-        i -= failure[i];
+        i = get_failure(state, i);
     }
     state->i = i;
     return result;
@@ -176,9 +260,17 @@ int mmatch_stream(mmatch_state *state, int t_pred, int j) {
         mmatch_state *state - The state to free
 */
 void mmatch_free(mmatch_state *state) {
-    free(state->failure);
     free(state->k);
-    if (state->period) free(state->c);
+    if (state->period) {
+        free(state->c);
+        while (state->failure->pred != NULL) state->failure = state->failure->pred;
+        while (state->failure->succ != NULL) {
+            state->failure = state->failure->succ;
+            free(state->failure->pred);
+        }
+    } else {
+        free(state->failure_table);
+    }
 }
 
 #endif
